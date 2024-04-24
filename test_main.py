5be6a7e8536d3c48c39b67dd4b9947f69f4db66b
Mihail Genzik <genziks@inbox.lv>
2024-04-24 17:57:41 +0300

    v4

--- test_main.py
+++ test_main.py
@@ -1,49 +1,55 @@
-from main import monyadd
-from main import mony_withdr
-from main import pass_check
 from main import account_creation
-from main import trans_filtering_logics
 from main import apply_trans
-
+from main import money_withdr
+from main import moneyadd
+from main import pass_check
+from main import trans_filtering_logics
 
 transactions = {
-   "Ot Vasi": 100,
-   "Ot Juri": 200
- }
+    "Ot Vasi": 100,
+    "Ot Juri": 200
+}
 
 
 def test_apply_trans_small_limit():
     assert apply_trans(100, 160, {
-   "Ot Vasi": 100,
-   "Ot Juri": 200
- }) == 100
+        "Ot Vasi": 100,
+        "Ot Juri": 200,
+        "Ot Sergeja dolg": 200
+    }) == 100
+
 
 def test_apply_trans():
     assert apply_trans(100, 400, {
-   "Ot Vasi": 100,
-   "Ot Juri": 200
- }) == 400
+        "Ot Vasi": 100,
+        "Ot Juri": 200
+    }) == 400
+
 
 def test_trans_filtering_logics():
     assert trans_filtering_logics(transactions, 150) == True
 
+
 def test_monyadd():
-    assert monyadd(1,2, 6) == 3
+    assert moneyadd(1, 2, 6) == 3
+
 
 def test_monyadd_smallLimit():
-    assert monyadd(1, 2, 2) == 1
+    assert moneyadd(1, 2, 2) == 1
+
 
 def test_money_wthdrw_too_little_ballance():
-    assert mony_withdr(9, 100) == 9
+    assert money_withdr(9, 100) == 9
 
 
 def test_money_wthdrw():
-    assert mony_withdr(12, 4) == 8
+    assert money_withdr(12, 4) == 8
 
 
 def test_pass_check():
     assert pass_check("zzz", "zzzz") == False
     assert pass_check("zzz", "zzz") == True
+
 
 def test_account_creation():
     assert account_creation() == True
